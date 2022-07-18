a81b05ec8d7b9a0720e5f095beed77962d62a4b9
ElisaTorelli <elisa.torelli1@gmail.com>
2022-07-18 18:39:46 +0200

    add code snippet

--- react-app/src/components/loginCard/LoginCard.tsx
+++ react-app/src/components/loginCard/LoginCard.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styles from '../loginCard/loginCard.module.css'
 
 function LoginCard() {
-  return <div>
-    <input type='text' placeholder="text" />
-    <button>Submit</button>
+  return <div className={styles.mainContainer}>
+    {/* <input type='text' placeholder="text" />
+    <button>Submit</button> */}
+  {/* card container */}
+  <h2>Login with your credentials: </h2>
+  <div className={styles.cardContainer}>
+    <input type='text' placeholder="email here..." className={styles.firstInput}/>
+    <input type='text' placeholder="password here..." className={styles.secondInput}/>
+    <div className={styles.btnContainer}>
+      <button>Submit</button>
+      <button>Register Now</button>
+    </div>
+  </div>
   </div>;
 }
 
