3fa1f8f9906ce85b5fa4a6376d0ddd66f8ed5d9b
Elisa <elisa.torelli1@gmail.com>
2022-07-19 13:09:26 +0200

    add code snippet

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import { useNavigate} from 'react-router-dom'
@@ -21,9 +21,18 @@ export let usersArray = [
 //   navigate:
 // }
 
+
 function App() {
+
+  // const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   document.title = `status is: ${passwordLength} `;
+  // })
+
   return <div className="App">
     <LoginCard />
+    {/* <p>the status is: {passwordLength}</p>
+    <button placeholder="click me" onClick={()=> setPasswordLength(passwordLength = 'true')}></button> */}
   </div>;
 }
 
